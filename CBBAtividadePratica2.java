008c3343c6a10b5edfa03bdd2127aaa9c6c2406b
João Victor <86197823+Sr1515@users.noreply.github.com>
2023-08-30 08:10:39 -0300

    CBBAtividadePratica2

---
+++ CBBAtividadePratica2.java
@@ -0,0 +1,15 @@
+// Questão 2 CBB
+
+public class CBBAtividadePratica2 {
+
+    public static void main(String[] args){
+        int valor1 = 100;
+        int valor2 = 80;
+        int valor3 = 50;
+
+        int soma = valor1 + valor2 + valor3;
+        int media = soma / 3;
+
+        System.out.println("A média dos valores é: " + media);
+    }
+}
