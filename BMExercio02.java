c0048235da22028c26622d718f3921ba1565bd69
Jose Sicupira <72612627+JoseNeto5689@users.noreply.github.com>
2023-08-30 08:16:17 -0300

    BMExercio02

---
+++ BMExercio02.java
@@ -0,0 +1,12 @@
+class BMExercio02 {
+
+    public static void main (String[] args){
+        int i = 1;
+        long j = 2;
+        float p = 20.0f;
+        double q = 30.0;
+        boolean b = true;
+        char c = "k";
+    } 
+
+}
